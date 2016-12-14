6f32c8a5a724f52ff3f147d4a73ef7b962b848f3
feizheng <1290657123@qq.com>
2016-12-14 15:22:57 +0800

    finish swiper

--- src/components/react-swiper.js
+++ src/components/react-swiper.js
@@ -1,21 +1,43 @@
 import './style.scss';
 import classNames from 'classnames';
 import Swipeable from 'react-swipeable';
+import Measure from 'react-measure';
+
 
 export default class extends React.Component{
   static propTypes = {
     cssClass:React.PropTypes.string,
     items:React.PropTypes.array,
     height:React.PropTypes.string,
+    duration:React.PropTypes.number,
+    dot:React.PropTypes.bool,
+    itemTemplate:React.PropTypes.func,
   };
 
   static defaultProps = {
-    items:[]
+    items:[],
+    duration:0.3,
+    dot:true,
+    itemTemplate:function(item,index){
+      return (
+        <img src={item.url} />
+      );
+    }
   };
 
   constructor(props){
     super(props);
     this.fomratItems();
+    this.state={
+      index:1,
+      dimensions:{},
+      translateX:0,
+      duration:props.duration
+    }
+  }
+
+  componentWillMount(){
+    this.toIndex(1);
   }
 
   fomratItems(){
@@ -26,27 +48,116 @@ export default class extends React.Component{
     items.unshift(last);
   }
 
-  initialTranslate(){
-
+  _onSwipedLeft(ev){
+    var index = this.state.index + 1;
+    this.toIndex(index);
+    // ev.preventDefault();
   }
 
+  _onSwipedRight(ev){
+    var index = this.state.index - 1;
+    this.toIndex(index);
+    // ev.preventDefault();
+  }
+
+  _onSwipingLeft(ev,deltaX){
+    var _translateX = this.state.index * this.state.dimensions.width;
+    this.setState({
+      duration:0,
+      translateX:`-${_translateX+deltaX}px`
+    });
+    // ev.preventDefault();
+  }
+
+  _onSwipingRight(ev,deltaX){
+    var _translateX = this.state.index * this.state.dimensions.width;
+    this.setState({
+      duration:0,
+      translateX:`-${_translateX-deltaX}px`
+    });
+    // ev.preventDefault();
+  }
+  toIndex(inIndex){
+    var index = this.getAbsIndex(inIndex);
+    this.setState({
+      index:index,
+      duration:this.props.duration,
+      translateX:`-${inIndex * 100/this.props.items.length}%`
+    });
+  }
+
+  _onTransitionEnd(){
+    this.jumpQuietly();
+  }
+
+  _dotClick(inIndex){
+    this.toIndex(inIndex);
+  }
+
+  getAbsIndex(inIndex){
+    var index = inIndex;
+    var length = this.props.items.length;
+    var _length = length - 1;
+
+    if(index == 0){
+      index = _length - 1;
+    }
+
+    if(index == _length){
+      index = 1;
+    }
+    return index;
+  }
+
+
+  jumpQuietly(){
+    var index = this.getAbsIndex(this.state.index);
+    this.setState({
+      index:index,
+      duration:0,
+      translateX:`-${index * 100/this.props.items.length}%`
+    });
+  }
+
+
   render(){
+    var dots = [];
+    var length = this.props.items.length - 2;
+    for (var i = 0; i < length; i++) {
+      dots.push(<span key={i} onClick={this._dotClick.bind(this,i+1)} data-active={this.state.index == (i+1)}></span>);
+    }
+
     return (
-      <div style={this.props.style} className={classNames('react-swiper',this.props.cssClass)}>
-        <Swipeable>
-          <div className="react-swiper-wrapper">
-            <ul className="react-swiper-scroller" style={{width:`${this.props.items.length*100}%`,height:this.props.height}}>
-              {this.props.items.map(function(item,index){
-                return (
-                  <li key={index} style={{ width:`${100/this.props.items.length}%`,height:this.props.height}}>
-                    <img src={item.url} />
-                  </li>
-                )
-              }.bind(this))}
-            </ul>
-          </div>
-        </Swipeable>
-      </div>
+      <Measure onMeasure={(dimensions) => {
+          this.setState({dimensions})
+        }}>
+        <div className={classNames('react-swiper',this.props.cssClass)}>
+          {this.props.dot ? <div className="react-swiper-dots">{dots}</div>: null}
+          <Swipeable flickThreshold={0.2} delta={10} preventDefaultTouchmoveEvent className="react-swiper-wrapper"
+            onSwipingLeft={this._onSwipingLeft.bind(this)}
+            onSwipingRight={this._onSwipingRight.bind(this)}
+            onSwipedLeft={this._onSwipedLeft.bind(this)}
+            onSwipedRight={this._onSwipedRight.bind(this)}
+            >
+              <ul className="react-swiper-scroller"
+                onTransitionEnd={this._onTransitionEnd.bind(this)}
+                style={{
+                  width:`${this.props.items.length*100}%`,
+                  height:this.props.height,
+                  transition:`transform ${this.state.duration}s`,
+                  transform:`translateX(${this.state.translateX})`
+                }}>
+                {this.props.items.map(function(item,index){
+                  return (
+                    <li key={index} style={{ width:`${100/this.props.items.length}%`,height:this.props.height}}>
+                      {this.props.itemTemplate(item,index)}
+                    </li>
+                  )
+                }.bind(this))}
+              </ul>
+          </Swipeable>
+        </div>
+      </Measure>
     );
   }
 }
--- src/dev.js
+++ src/dev.js
@@ -3,25 +3,65 @@ import ReactSwiper from './main';
 
 
 class App extends React.Component {
+  _getItemTemplate(item,index){
+    return (
+      <a href={item.href}>
+        <img src={item.url} data-index={index} />
+      </a>
+    )
+  }
+
   render(){
     return (
-      <ReactSwiper
-        height='270px'
-        items={[
-        {
-            url:require('./assets/1.jpg'),
-            index:1
-        },
-        {
-            url:require('./assets/2.jpg'),
-            index:2
-        },
-        {
-            url:require('./assets/3.jpg'),
-            index:3
-        }
-      ]}>
-      </ReactSwiper>
+      <div class="hello-swiper">
+        <ReactSwiper
+          height='270px'
+          itemTemplate={this._getItemTemplate.bind(this)}
+          items={[
+          {
+              url:require('./assets/1_s.jpg'),
+              href:'#1',
+              index:1
+          },
+          {
+              url:require('./assets/2_s.jpg'),
+              href:'#2',
+              index:2
+          },
+          {
+              url:require('./assets/3_s.jpg'),
+              href:'#3',
+              index:3
+          },
+          {
+              url:require('./assets/4_s.jpg'),
+              href:'#4',
+              index:4
+          }
+        ]}>
+        </ReactSwiper>
+        <ReactSwiper
+          height='200px'
+          dot={false}
+          items={[
+          {
+              url:require('./assets/4_s.jpg'),
+              href:'#1',
+              index:1
+          },
+          {
+              url:require('./assets/5_s.jpg'),
+              href:'#2',
+              index:2
+          },
+          {
+              url:require('./assets/6_s.jpg'),
+              href:'#3',
+              index:3
+          }
+        ]}>
+        </ReactSwiper>
+      </div>
     );
   }
 }
